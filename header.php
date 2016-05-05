7de743fe26d01898db8291a9b0d44a3a3e64ecf0
jankavi85 <jankavi85@gmail.com>
2016-05-05 21:39:20 +0530

    common header

--- header.php
+++ header.php
@@ -1,12 +1,12 @@
 <!DOCTYPE html>
-<html>
-
+<html lang="en">
 <head>
+	
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta name="description" content="">
     <meta name="author" content="">
-    <title>Home | Lansu.lk</title>
+    <title>Lansu.lk</title>
     <link href="css/bootstrap.min.css" rel="stylesheet">
     <link href="css/font-awesome.min.css" rel="stylesheet">
     <link href="css/prettyPhoto.css" rel="stylesheet">
@@ -24,84 +24,27 @@
     <link rel="apple-touch-icon-precomposed" sizes="72x72" href="images/ico/apple-touch-icon-72-precomposed.png">
     <link rel="apple-touch-icon-precomposed" href="images/ico/apple-touch-icon-57-precomposed.png">
 </head><!--/head-->
+
 <body>
-<header id="header"><!--header-->
-		<div class="header_top"><!--header_top-->
-			<div class="container">
-				<div class="row">
-					<div class="col-sm-6">
-						<div class="contactinfo">
-							<ul class="nav nav-pills">
-								<li><a href="#"><i class="fa fa-phone"></i> +2 95 01 88 821</a></li>
-								<li><a href="#"><i class="fa fa-envelope"></i> info@Lansulk.com</a></li>
-							</ul>
-						</div>
-					</div>
-					<div class="col-sm-6">
-						<div class="social-icons pull-right">
-							<ul class="nav navbar-nav">
-								<li><a href="#"><i class="fa fa-facebook"></i></a></li>
-								<li><a href="#"><i class="fa fa-twitter"></i></a></li>
-								<li><a href="#"><i class="fa fa-linkedin"></i></a></li>
-								<li><a href="#"><i class="fa fa-dribbble"></i></a></li>
-								<li><a href="#"><i class="fa fa-google-plus"></i></a></li>
-							</ul>
-						</div>
-					</div>
-				</div>
-			</div>
-		</div><!--/header_top-->
-		
+	
+	<header id="header"><!--header-->
+			
 		<div class="header-middle"><!--header-middle-->
 			<div class="container">
 				<div class="row">
-					<div class="col-sm-8">
-						<div class="row">
-							<div class="col-sm-3">
-								<div class="logo pull-left">
-									<a href="index.php"><img src="images/home/logo.png" alt="" /></a>
-								</div>
-							</div>
-							<div class="col-sm-9">
-								<div class="header-bottom"><!--header-bottom-->								
-									<div class="navbar-header">
-										<button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
-											<span class="sr-only">Toggle navigation</span>
-											<span class="icon-bar"></span>
-											<span class="icon-bar"></span>
-											<span class="icon-bar"></span>
-										</button>
-									</div>
-									<div class="mainmenu pull-left">
-										<ul class="nav navbar-nav collapse navbar-collapse">
-											<li><a href="index.php" class="active">Home</a></li>
-											<li class="dropdown"><a href="#">Shop<i class="fa fa-angle-down"></i></a>
-			                                    <ul role="menu" class="sub-menu">
-			                                        <li><a href="shop.php">Products</a></li>
-													<li><a href="product-details.php">Product Details</a></li> 
-													<li><a href="checkout.php">Checkout</a></li> 
-													<li><a href="cart.php">Cart</a></li> 
-													<li><a href="login.php">Login</a></li> 
-			                                    </ul>
-			                                </li> 
-											<li class="dropdown"><a href="#">Blog<i class="fa fa-angle-down"></i></a>
-			                                    <ul role="menu" class="sub-menu">
-			                                        <li><a href="blog.php">Blog List</a></li>
-													<li><a href="blog-single.php">Blog Single</a></li>
-			                                    </ul>
-			                                </li> 
-											<li><a href="404.php">404</a></li>
-											<li><a href="contact-us.php">Contact</a></li>
-										</ul>
-									</div><!--/header-bottom-->								
-								</div>
-							</div>
-						</div>
-					</div>
 					<div class="col-sm-4">
+						<div class="logo pull-left">
+							<a href="index.php"><img src="images/home/logo.png" alt="" /></a>
+						</div>
+						
+					</div>
+					<div class="col-sm-8">
 						<div class="shop-menu pull-right">
 							<ul class="nav navbar-nav">
-								<li><a href="google.com"><i class="fa fa-user"></i> Account</a></li>
+								<li><a href="account.php"><i class="fa fa-user"></i> Account</a></li>
+								<li><a href="#"><i class="fa fa-star"></i> Wishlist</a></li>
+								<li><a href="checkout.php"><i class="fa fa-crosshairs"></i> Checkout</a></li>
+								<li><a href="cart.php"><i class="fa fa-shopping-cart"></i> Cart</a></li>
 								<li><a href="login.php"><i class="fa fa-lock"></i> Login</a></li>
 							</ul>
 						</div>
@@ -109,6 +52,48 @@
 				</div>
 			</div>
 		</div><!--/header-middle-->
+	
+		<div class="header-bottom"><!--header-bottom-->
+			<div class="container">
+				<div class="row">
+					<div class="col-sm-9">
+						<div class="navbar-header">
+							<button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
+								<span class="sr-only">Toggle navigation</span>
+								<span class="icon-bar"></span>
+								<span class="icon-bar"></span>
+								<span class="icon-bar"></span>
+							</button>
+						</div>
+						<div class="mainmenu pull-left">
+							<ul class="nav navbar-nav collapse navbar-collapse">
+								<li><a href="index.php" class="active">Home</a></li>
+								<li class="dropdown"><a href="#">Shop<i class="fa fa-angle-down"></i></a>
+                                    <ul role="menu" class="sub-menu">
+                                        <li><a href="shop.php">Products</a></li>
+										<li><a href="product-details.php">Product Details</a></li> 
+										<li><a href="checkout.php">Checkout</a></li> 
+										<li><a href="cart.php">Cart</a></li> 
+										<li><a href="login.php">Login</a></li> 
+                                    </ul>
+                                </li> 
+								<li class="dropdown"><a href="#">Blog<i class="fa fa-angle-down"></i></a>
+                                    <ul role="menu" class="sub-menu">
+                                        <li><a href="blog.php">Blog List</a></li>
+										<li><a href="blog-single.php">Blog Single</a></li>
+                                    </ul>
+                                </li> 
+								<li><a href="404.php">404</a></li>
+								<li><a href="contact-us.php">Contact</a></li>
+							</ul>
+						</div>
+					</div>
+					<div class="col-sm-3">
+						<div class="search_box pull-right">
+							<input type="text" placeholder="Search"/>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div><!--/header-bottom-->
 	</header><!--/header-->
-	</body>
-	</html>
